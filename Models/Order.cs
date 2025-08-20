b644abca5ce1ae8dcd7304b60b1cd5c8edabc92b
Sean <sean.to@websparks.sg>
2025-08-20 10:14:52 +0700

    Update Order model class

--- Models/Order.cs
+++ Models/Order.cs
@@ -2,9 +2,9 @@ namespace LegacyOrderService.Models
 {
     public class Order
     {
-        public string CustomerName;
-        public string ProductName;
-        public int Quantity;
-        public double Price;
+        public string CustomerName { get; set; } = String.Empty;
+        public string ProductName { get; set; } = String.Empty;
+        public int Quantity { get; set; }
+        public double Price { get; set; }
     }
 }
